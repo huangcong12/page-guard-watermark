60d49c3a9a2cd76a70347a230661080680c99f1b
dirty_pig <huangcong12@gmail.com>
2023-11-22 16:52:13 +0800

    增加一个绿色的Upgrade to Pro 按钮

--- src/AdminMenuHandler.php
+++ src/AdminMenuHandler.php
@@ -23,6 +23,28 @@ class AdminMenuHandler
         self::subMenus();
         // 调整首个子菜单名字
         self::customizeWatermarkListSubmenuName();
+
+        // 修改按钮颜色
+        self::adjustProMenuItem();
+        add_action('admin_head', "LitaPig\\PageGuardWatermark\\AdminMenuHandler::admin_menu_styles", 11);
+    }
+
+    /**
+     * Output inline styles for the admin menu.
+     *
+     * @since 1.7.8
+     */
+    public static function admin_menu_styles()
+    {
+
+//        $styles = '#adminmenu .wpforms-menu-new { color: #f18500; vertical-align: super; font-size: 9px; font-weight: 600; padding-left: 2px; }';
+
+//        if (!wpforms()->is_pro()) {
+        $styles = 'a.page-guard-watermark-sidebar-upgrade-pro { background-color: #00a32a !important; color: #fff !important; font-weight: 600 !important; }';
+//        }
+
+        // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+        printf('<style>%s</style>', $styles);
     }
 
     /**
@@ -35,7 +57,7 @@ class AdminMenuHandler
             "PageMark",
             'manage_options',
             self::TOP_MENU_SLUG,
-            [__NAMESPACE__ . '\Controller\PageGuardWatermark', 'list'],
+            [__NAMESPACE__ . '\Controller\PageGuardWatermark', 'edit'],
             'dashicons-images-alt',
             20
         );
@@ -47,13 +69,33 @@ class AdminMenuHandler
     private static function subMenus()
     {
         add_submenu_page(self::TOP_MENU_SLUG,
-            "List",
-            "List",
+            "Upgrade to Pro",
+            "Upgrade to Pro",
             "manage_options",
-            "page_guard_watermark_list",
-            [__NAMESPACE__ . '\Controller\PageGuardWatermark', 'list'],
+            "page_guard_watermark_upgrade_to_pro",
+            [__NAMESPACE__ . '\Controller\UpgradeToPro', 'show'],
         );
     }
+
+    /**
+     * 给“Upgrade to Pro” 按钮增加一个 class，方便上颜色
+     */
+    private static function adjustProMenuItem()
+    {
+        global $submenu;
+        $upgradeButtonPosition = count($submenu[self::TOP_MENU_SLUG]) - 1;
+        if ($upgradeButtonPosition < 1) {
+            return;
+        }
+
+        if (isset($submenu[self::TOP_MENU_SLUG][$upgradeButtonPosition][4])) {
+            echo 11;
+            $submenu[self::TOP_MENU_SLUG][$upgradeButtonPosition][4] .= ' page-guard-watermark-sidebar-upgrade-pro';
+        } else {
+            $submenu[self::TOP_MENU_SLUG][$upgradeButtonPosition][] = 'page-guard-watermark-sidebar-upgrade-pro';
+        }
+    }
+
 
     /**
      * 当有子菜单时 wordpress 默认把主菜单复制一份为子菜单。主、子菜单同名会不协调，因此这里改名
@@ -64,7 +106,7 @@ class AdminMenuHandler
 
         // 更改第一个子菜单的名称
         if (isset($submenu[LITA_PIG_PGW_WP_NAME])) {
-            $submenu[LITA_PIG_PGW_WP_NAME][0][0] = 'All Watermark';
+            $submenu[LITA_PIG_PGW_WP_NAME][0][0] = 'Watermark Settings';
         }
     }
 }
